da223486657f5e237c01b10f6517bf81e2ef5869
wayabi <wayabi.software@gmail.com>
2019-10-02 16:09:14 +0900

    add comment

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,27 @@
 
 int main(){
 	sm sm_;
+	//add_event<source_state, event, next_state>
 	sm_.add_event<sm_state_init, e_1, sm_state_start>();
+	sm_.add_event<sm_state_init, e_2, sm_state_init>();
+	sm_.add_event<sm_state_start, e_3, sm_state_init>();
+	sm_.add_event<sm_state_start, e_4, sm_state_start>();
+
 	sm_.set_initial_state<sm_state_init>();
+
+	//call (super_state:sm_state).on_update()
+	sm_.update();
+
+	sm_.process_event(std::make_shared<e_1>());
+	sm_.update();
+	sm_.process_event(std::make_shared<e_4>());
+	sm_.update();
+	sm_.process_event(std::make_shared<e_3>());
+	sm_.update();
+	sm_.process_event(std::make_shared<e_2>());
 	sm_.update();
 	sm_.process_event(std::make_shared<e_1>());
 	sm_.update();
 
-	printf("aa\n");
 	return 0;
 }
